61b3093e4d584d49cc944c70d3ea0c55214f58f3
Mattareeya Rach-M <matzeeya@gmail.com>
2022-06-24 21:53:16 +0700

    add icon font awa 6.1.1

--- resources/views/includes/front-end/sidebar.blade.php
+++ resources/views/includes/front-end/sidebar.blade.php
@@ -1,5 +1,6 @@
-<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;height:100%;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <i class="fa-brands fa-elementor fa-2xl"></i>
       <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
       <span class="fs-4">Menu</span>
     </a>
@@ -7,21 +8,24 @@
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
         <a href="#" class="nav-link active" aria-current="page">
+          <i class="fa-solid fa-calendar-days fa-lg"></i>
           <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"/></svg>
           ปฏิทินการใช้รถ
         </a>
       </li>
       <li>
         <a href="#" class="nav-link link-dark">
+        <i class="fa-solid fa-address-card fa-lg"></i>
           <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
           รายการจองของฉัน
         </a>
       </li>
       <li>
         <a href="#" class="nav-link link-dark">
+          <i class="fa-solid fa-car fa-lg"></i>
           <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"/></svg>
           รายละเอียดรถ
         </a>
       </li>
     </ul>
-  </div>
+</div>
--- resources/views/layouts/front-end/default.blade.php
+++ resources/views/layouts/front-end/default.blade.php
@@ -7,25 +7,31 @@
   <meta name="format-detection" content="telephone=no">
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/fontawesome.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
   <title> @yield('title')</title>
   @yield('css_script')
-
+  <style>
+    /*.section {
+      height: 100%;
+    }*/
+  </style>
 </head>
 <body>
 @include('includes.front-end.header')
 
 <main><!-- start main -->
   <section class="container-fluid">
-  <div class="row">
-    @if(Auth::user()!=null)
-    <div class="col-sm-3">
-        @include('includes.front-end.sidebar')
+    <div class="row">
+      @if(Auth::user()!=null)
+      <div class="col-sm-3">
+          @include('includes.front-end.sidebar')
+      </div>
+      @endif
+      <div class="col">
+          @yield('content')
+      </div>
     </div>
-    @endif
-    <div class="col">
-        @yield('content')
-    </div>
-  </div>
   </section>
 </main><!-- end main -->
 <!-- Footer - start -->
